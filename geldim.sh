f5b4b5c2de9b268d3cc2f51abda311087c6684c3
Dima Gelfer <geldim@gmail.com>
2020-05-04 00:17:20 +0300

    update

--- geldim.sh
+++ geldim.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-echo test
+if [ "$1" -eq "ssh" ]; then
+    GITHUB_USER=geldim
+    TARGET_FILE=$HOME/.ssh/authorized_keys
+    
+    echo "Allowing github user $GITHUB_USER to authorize as $(whoami) on this computer"
+    
+    curl -s https://github.com/$GITHUB_USER.keys >> $TARGET_FILE
+fi
